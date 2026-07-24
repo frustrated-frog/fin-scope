35f657fb7f766dea5d31b820cafc353c2fa3652a
machengqian <394690286@qq.com>
2026-07-24 21:55:30 +0800

    代码格式修改

--- backend/finscope-rpc/src/main/java/com/finscope/rpc/marketintel/DragonTigerData.java
+++ backend/finscope-rpc/src/main/java/com/finscope/rpc/marketintel/DragonTigerData.java
@@ -3,6 +3,7 @@ package com.finscope.rpc.marketintel;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.finscope.domain.marketintel.DragonTigerRecord;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * Provider 规范化后的龙虎榜记录及数据质量告警。
  */
+@Data
 public final class DragonTigerData {
     private final List<DragonTigerRecord> records;
     private final List<String> warnings;
@@ -21,14 +23,6 @@ public final class DragonTigerData {
             @JsonProperty("warnings") List<String> warnings) {
         this.records = immutable(records);
         this.warnings = immutable(warnings);
-    }
-
-    public List<DragonTigerRecord> getRecords() {
-        return records;
-    }
-
-    public List<String> getWarnings() {
-        return warnings;
     }
 
     private static <T> List<T> immutable(List<T> values) {
