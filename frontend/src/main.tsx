88126c79bb0ba7fac58c61a0c33a0b8ba40927cc
machengqian <394690286@qq.com>
2026-09-08 01:37:55 +0800

    style: 统一全站卡片与面板透明材质

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './shared/visuals/fluid/flow.css';
 import './app/AppShell.css';
+import './app/WorkspaceMaterials.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
